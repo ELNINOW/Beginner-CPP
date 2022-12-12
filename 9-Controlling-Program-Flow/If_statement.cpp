0173ee9b2803cdb787eff36fcb89a073654060d8
Kalana Madusara <Kalana.epc@gmail.com>
2022-12-13 00:56:32 +0530

    If statement sample

---
+++ 9-Controlling-Program-Flow/If_statement.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+
+int num1{},num2{};
+
+cout <<boolalpha;
+cout<<"enter 2 integers seperated by a space";
+cin>> num1 >> num2;
+
+
+cout << num1 << num2;
+
+    return (0);
+}
+ 
